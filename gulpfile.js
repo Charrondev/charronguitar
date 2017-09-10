f8f06a7e01da84554469a7bb2b50e48ca5bcd73a
Adam Charron <adam@charrondev.com>
2017-09-09 22:57:52 -0400

    Upgrade build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,74 @@
 const gulp = require("gulp");
+const babel = require("babelify");
+const watchify = require("watchify");
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const sourcemaps = require("gulp-sourcemaps");
 const browserSync = require("browser-sync").create();
 const sass = require("gulp-sass");
+const cssnano = require("gulp-cssnano");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 
-// Static Server + watching scss/html files
-gulp.task("serve", ["sass"], function() {
+function swallowError(error) {
+    // If you want details of the error in the console
+    console.log(error.toString());
+
+    this.emit("end");
+}
+
+function compile(watch) {
+    var bundler = watchify(
+        browserify("./src/js/app.js", { debug: true }).transform(babel)
+    );
+
+    function rebundle() {
+        bundler
+            .bundle()
+            .on("error", swallowError)
+            .pipe(source("app.js"))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            .pipe(sourcemaps.write("./"))
+            .pipe(gulp.dest("dist/js"));
+    }
+
+    if (watch) {
+        bundler.on("update", function() {
+            console.log("-> bundling...");
+            rebundle();
+            browserSync.reload();
+        });
+    }
+
+    rebundle();
+}
+
+function watch() {
+    return compile(true);
+}
+
+gulp.task("bundle", () => compile());
+
+gulp.task("bundle-watch", () => watch());
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task("sass", () => {
+    return gulp
+        .src("src/scss/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .on("error", swallowError)
+        .pipe(postcss([autoprefixer()]))
+        .pipe(cssnano())
+        .pipe(gulp.dest("build"))
+        .pipe(browserSync.stream())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("dist/css"));
+});
+
+gulp.task("watch", ["sass", "bundle"], () => {
     browserSync.init({
         server: {
             baseDir: "dist",
@@ -13,18 +76,11 @@ gulp.task("serve", ["sass"], function() {
         }
     });
 
+    gulp.watch("src/js/*.js", ["bundle"]);
     gulp.watch("src/scss/*.scss", ["sass"]);
     gulp.watch("dist/*.html").on("change", browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task("sass", function() {
-    return gulp
-        .src("src/scss/*.scss")
-        .pipe(sass())
-        .pipe(postcss([autoprefixer()]))
-        .pipe(gulp.dest("dist/css"))
-        .pipe(browserSync.stream());
-});
+gulp.task("default", ["watch"], () => {});
 
-gulp.task("default", ["serve"]);
+gulp.task("build", ["bundle", "sass"]);
